5ef5e1f932e8769a4464e14e20e5917e54d4a2d7
Haresh Khanna <khanna.haresh@yahoo.co.in>
2016-12-13 11:30:26 +0530

    Makes profile work using retrofit and picasso

--- app/src/main/java/com/sdsmdg/hareshkh/githubapp/tabs/Profile.java
+++ app/src/main/java/com/sdsmdg/hareshkh/githubapp/tabs/Profile.java
@@ -5,10 +5,21 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sdsmdg.hareshkh.githubapp.R;
+import com.sdsmdg.hareshkh.githubapp.data.models.profile.ProfileModel;
+import com.sdsmdg.hareshkh.githubapp.data.remotes.ProfileApi;
+import com.squareup.picasso.Picasso;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class Profile extends Fragment {
+    private TextView name, place;
+    private ImageView profilePhoto;
 
     public Profile() {
         // Required empty public constructor
@@ -21,7 +32,28 @@ public class Profile extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View inflatedView = inflater.inflate(R.layout.fragment_profile, container, false);
+        name = (TextView) inflatedView.findViewById(R.id.name);
+        place = (TextView) inflatedView.findViewById(R.id.place);
+        profilePhoto = (ImageView) inflatedView.findViewById(R.id.profilePhoto);
+        change();
+        return inflatedView;
     }
+
+    public void change() {
+        ProfileApi.Factory.getInstance().getProfileModel().enqueue(new Callback<ProfileModel>() {
+            @Override
+            public void onResponse(Call<ProfileModel> call, Response<ProfileModel> response) {
+                name.setText(response.body().getName());
+                place.setText(response.body().getLocation());
+                Picasso.with(getContext()).load(response.body().getAvatarUrl()).into(profilePhoto);
+            }
+
+            @Override
+            public void onFailure(Call<ProfileModel> call, Throwable t) {
+
+            }
+        });
+    }
+
 }
