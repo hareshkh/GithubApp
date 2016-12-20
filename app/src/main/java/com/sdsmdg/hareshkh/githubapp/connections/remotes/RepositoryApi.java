3f15f5a922154b63495fb57fcd6b878438cece64
Haresh Khanna <khanna.haresh@yahoo.co.in>
2016-12-20 16:43:48 +0530

    Adds POJO and Retrofit interface for Notifications

---
+++ app/src/main/java/com/sdsmdg/hareshkh/githubapp/connections/remotes/RepositoryApi.java
@@ -0,0 +1,37 @@
+package com.sdsmdg.hareshkh.githubapp.connections.remotes;
+
+import com.sdsmdg.hareshkh.githubapp.connections.models.repository.RepositoryModel;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface RepositoryApi {
+
+    String BASE_URL = "https://api.github.com/";
+
+    @GET("user/repos")
+    Call<List<RepositoryModel>> getRepositryModel(
+            @Query("access_token") String token,
+            @Query("affiliation") String affiliations
+    );
+
+    class Factory {
+        public static RepositoryApi service;
+
+        public static RepositoryApi getInstance() {
+            if (service == null) {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).build();
+                service = retrofit.create(RepositoryApi.class);
+                return service;
+            } else {
+                return service;
+            }
+        }
+    }
+}
