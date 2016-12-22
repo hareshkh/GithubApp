f9e22276f0aad8d3883610aa935d572048c0e2d2
Haresh Khanna <khanna.haresh@yahoo.co.in>
2016-12-22 23:58:42 +0530

    Adds NotificationApi Interface

---
+++ app/src/main/java/com/sdsmdg/hareshkh/githubapp/connections/remotes/NotificationApi.java
@@ -0,0 +1,32 @@
+package com.sdsmdg.hareshkh.githubapp.connections.remotes;
+
+import com.sdsmdg.hareshkh.githubapp.connections.models.profile.ProfileModel;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface NotificationApi {
+
+    String BASE_URL = "https://api.github.com/";
+
+    @GET("notifications")
+    Call<NotificationModel> getNotificationModel(@Query("access_token") String token);
+
+    class Factory {
+        public static NotificationApi service;
+
+        public static NotificationApi getInstance() {
+            if (service == null) {
+                Retrofit retrofit = new Retrofit.Builder()
+                        .addConverterFactory(GsonConverterFactory.create()).baseUrl(BASE_URL).build();
+                service = retrofit.create(NotificationApi.class);
+                return service;
+            } else {
+                return service;
+            }
+        }
+    }
+}
